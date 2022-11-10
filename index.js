e56866542de7e994a0a0f86f27f98db5d4e15eb4
Taron25 <taron.abrahamyan.2018@mail.ru>
2022-11-10 23:45:23 +0400

    for-cikl

--- index.js
+++ index.js
@@ -37,3 +37,92 @@ let section = document.querySelector("section");
 section.style.backgroundColor = "#F1F5F8";
 
 
+let allbtn = document.querySelector(".btn1");
+let breakfastbtn = document.querySelector(".btn2");
+let lunchbtn = document.querySelector(".btn3");
+let shakesbtn = document.querySelector(".btn4");
+let dinnerbtn = document.querySelector(".btn5");
+
+let all = document.querySelector(".all");
+let breakfast = document.querySelectorAll(".breakfast");
+let lunch = document.querySelectorAll(".lunch");
+let shakes = document.querySelectorAll(".shakes");
+let dinner = document.querySelector(".dinner");
+
+allbtn.onclick = function () {
+    console.log(all, 'all');
+    all.style.display = "block"
+    for (let i = 0; i < breakfast.length; i++) {
+        breakfast[i].style.display = "block"
+    }
+    for (let i = 0; i < lunch.length; i++) {
+        lunch[i].style.display = "block"
+    }
+    for (let i = 0; i < shakes.length; i++) {
+        shakes[i].style.display = "block"
+    }
+   {
+        dinner.style.display = "block"
+    }
+}
+
+breakfastbtn.onclick = function () {
+    console.log(breakfast, 'breakfast');
+    for (let i = 0; i < breakfast.length; i++) {
+        breakfast[i].style.display = "block"
+    }
+    for (let i = 0; i < lunch.length; i++) {
+        lunch[i].style.display = "none"
+    }
+    for (let i = 0; i < shakes.length; i++) {
+        shakes[i].style.display = "none"
+    }
+   
+        dinner.style.display = "none"
+    }
+
+lunchbtn.onclick = function () {
+    console.log(lunch, 'lunch');
+    for (let i = 0; i < breakfast.length; i++) {
+        breakfast[i].style.display = "none"
+    }
+    for (let i = 0; i < lunch.length; i++) {
+        lunch[i].style.display = "block"
+    }
+    for (let i = 0; i < shakes.length; i++) {
+        shakes[i].style.display = "none"
+    }
+    dinner.style.display = "none"
+}
+
+shakesbtn.onclick = function () {
+    console.log(shakes, 'shakes');
+    for (let i = 0; i < breakfast.length; i++) {
+        breakfast[i].style.display = "none"
+    }
+    for (let i = 0; i < lunch.length; i++) {
+        lunch[i].style.display = "none"
+    }
+    for (let i = 0; i < shakes.length; i++) {
+        shakes[i].style.display = "block"
+    }
+    dinner.style.display = "none"
+}
+
+dinnerbtn.onclick = function () {
+    console.log(dinner, 'dinner');
+    for (let i = 0; i < breakfast.length; i++) {
+        breakfast[i].style.display = "none"
+    }
+    for (let i = 0; i < lunch.length; i++) {
+        lunch[i].style.display = "none"
+    }
+    for (let i = 0; i < shakes.length; i++) {
+        shakes[i].style.display = "none"
+    }
+    
+    dinner.style.display = "block"
+
+}
+
+
